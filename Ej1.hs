1f88a949cc4eca9b8f19d09307fe92228a0b894b
Yerguer <dariohdez16@gmail.com>
2017-09-21 10:11:07 +0200

    Almost done

--- Ej1.hs
+++ Ej1.hs
@@ -3,8 +3,26 @@ module Ejercicios where
   x :: String
   x = "Cadena"
 
-  y :: Integer
-  y = 23
+  y :: Int
+  y = length x + 7
+
+  z :: Integer
+  z = 20
+
+  a :: Num a => a
+  a = 34
+
+  suma1 :: Integer -> Integer -> Integer
+  suma1 a b = a + b
+
+  suma2 :: Num a => a -> a -> a
+  suma2 a b = a + b
 
   main :: IO()
-  main = do print x
+  main = do { print x;
+            print y;
+            print a;
+            print (z,a);
+            print [z,a];
+            print (suma1 2 3);
+            print (suma2 23 10); }
