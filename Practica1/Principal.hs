51a0812370f3166a5eb7ab4a69ebceb9bccf310f
Yerguer <dariohdez16@gmail.com>
2017-09-24 21:23:31 +0200

    Changes from September 21st

--- Practica1/Principal.hs
+++ Practica1/Principal.hs
@@ -1,2 +1,16 @@
 module Principal where
- import Quick 
+  import Quick
+  import Data.List
+
+  var :: [Integer]
+  var = quicksort [3,1,5,9,2,6]
+
+  var2 :: [Integer]
+  var2 = quicksort [1,3,2]
+
+  var3 :: [Integer]
+  var3 = sort [5,2,9,8,1]
+
+  main = do print var
+            print var2
+            print var3
