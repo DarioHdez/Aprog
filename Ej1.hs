7990d5a02b93bc854c9e4be690a3b1b5e62f4608
Yerguer <dariohdez16@gmail.com>
2017-09-25 17:38:30 +0200

    Finishing the mission!

--- Ej1.hs
+++ Ej1.hs
@@ -1,4 +1,5 @@
 module Ejercicios where
+ {-# LANGUAGE Rank2Types #-}
 
   x :: String
   x = "Cadena"
@@ -12,17 +13,47 @@ module Ejercicios where
   a :: Num a => a
   a = 34
 
+ -- Tupla de numero polimorfico
+  nums1 :: Num a => (Integer, a)
+  nums1 = (z,a)
+
+  nums2 :: [Integer]
+  nums2 = [z,a]
+
   suma1 :: Integer -> Integer -> Integer
   suma1 a b = a + b
 
   suma2 :: Num a => a -> a -> a
   suma2 a b = a + b
 
-  main :: IO()
-  main = do { print x;
-            print y;
-            print a;
-            print (z,a);
-            print [z,a];
-            print (suma1 2 3);
-            print (suma2 23 10); }
+  --funcionTupla = (suma1 2 3, suma2 4 5)
+  funcionTupla :: Num a => (Integer -> Integer -> Integer, a -> a -> a)
+  funcionTupla = (suma1, suma2)
+
+  listaTupla :: [Integer -> Integer -> Integer]
+  listaTupla = [suma1, suma2]
+
+  type ListaEnteros = [Integer]
+  listaPrueba :: ListaEnteros
+  listaPrueba = [5,5,4,7,6]
+
+  data Numero = Entero Integer | Decimal Double
+  numEjemplo :: Numero
+  numEjemplo = Entero 3
+
+  type FuncionEntreEnteros = Integer -> Integer
+  suma1Parc :: FuncionEntreEnteros
+  suma1Parc  = suma1 2
+
+  prueba = suma1Parc 3
+
+  {--data Funcion a b where
+    F :: (Num a, Num b) => (a -> b) -> Funcion a b
+
+  f2 :: Funcion Integer Integer
+  f2 = F (suma1 8) --}
+
+  type Funcion' a b = (Num a, Num b) => a->b
+
+  f2' :: Funcion' Integer Integer
+  f2' = suma1 8
