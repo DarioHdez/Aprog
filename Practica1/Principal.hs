915bb5607005139a6d1ab488f43ff91ac4e18f5d
Yerguer <dariohdez16@gmail.com>
2017-09-28 14:17:39 +0200

    Ejercicio d terminado

--- Practica1/Principal.hs
+++ Practica1/Principal.hs
@@ -1,16 +1,27 @@
 module Principal where
   import Quick
   import Data.List
+  import GHC.List
 
-  var :: [Integer]
-  var = quicksort [3,1,5,9,2,6]
+  -- Ejercicio d
+  miReplicate :: Int -> a -> [a]
+  miReplicate 1 x = [x]
+  miReplicate n x = [x] ++ miReplicate (n-1) x
 
-  var2 :: [Integer]
-  var2 = quicksort [1,3,2]
+  -- replicate original:
+  --  replicate n x = take n (repeat x)
 
-  var3 :: [Integer]
-  var3 = sort [5,2,9,8,1]
+  miFilter :: (a -> Bool) -> [a] -> [a]
+  miFilter _pred []    = []
+  miFilter pred (x:xs)
+          | pred x         = x : filter pred xs
+          | otherwise      = filter pred xs
 
-  main = do print var
-            print var2
-            print var3
+  main :: IO()
+  main = do print (quicksort [3,1,5,9,2,6])
+            print (quicksort [1,3,2])
+            print (sort [5,2,9,8,1])
+            print (miReplicate 3 "Wololo")
+            print (miReplicate 3 True)
+            print (miReplicate 3 5)
+            print (miFilter (>5) [5,6,7,8])
