603fb5ebbe77c661fc7852bf7b6cfbab9f168f41
Yerguer <dariohdez16@gmail.com>
2017-09-28 14:36:06 +0200

    Ejercicio e

--- Practica1/Apartado e/Quick.hs
+++ Practica1/Apartado e/Quick.hs
@@ -2,5 +2,9 @@ module Quick where
 
   quicksort :: Ord t => [t] -> [t]
 
-  quicksort [] = []
-  quicksort (x:xs) = (quicksort [ y | y <- xs, y <= x ]) ++ [x] ++ (quicksort [ z | z <- xs, z  > x ])
+  --quicksort [] = []
+  quicksort (x:xs) = if xs == [] then error "La lista está vacia"
+                    else
+                      (quicksort [ y | y <- xs, y <= x ])
+                      ++ [x] ++
+                      (quicksort [ z | z <- xs, z  > x ])
