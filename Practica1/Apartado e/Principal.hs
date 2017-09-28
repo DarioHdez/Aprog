b7a072c7b3ef43d94d444cdf66516189ae62f456
Yerguer <dariohdez16@gmail.com>
2017-09-28 14:45:31 +0200

    Maybe añadido

--- Practica1/Apartado e/Principal.hs
+++ Practica1/Apartado e/Principal.hs
@@ -1,8 +1,6 @@
 module Principal where
   import Quick
-  import Data.List
   
-
   main :: IO()
   main = do print (quicksort [3,1,5,9,2,6])
             print (quicksort [1,3,2])
--- Practica1/Apartado e/Quick.hs
+++ Practica1/Apartado e/Quick.hs
@@ -1,10 +1,12 @@
 module Quick where
+  import Data.List
 
-  quicksort :: Ord t => [t] -> [t]
-
+  quicksort :: Ord t => [t] -> Maybe [t]
   --quicksort [] = []
-  quicksort (x:xs) = if xs == [] then error "La lista está vacia"
+  {-- quicksort (x:xs) = if xs == [] then error "La lista está vacia"
                     else
                       (quicksort [ y | y <- xs, y <= x ])
                       ++ [x] ++
-                      (quicksort [ z | z <- xs, z  > x ])
+                      (quicksort [ z | z <- xs, z  > x ]) --}
+  quicksort (x:xs) = if (length xs) < 3 then Nothing
+                     else Just (sort (x:xs))
