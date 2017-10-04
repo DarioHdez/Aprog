0f264d60b6daa6cc37859aab6160930071ecd95c
Yerguer <dariohdez16@gmail.com>
2017-10-04 14:05:18 +0200

    Parcial 1

---
+++ Parcial1.hs
@@ -0,0 +1,13 @@
+module Parcial1 where
+
+  calcular :: (Int -> Int -> Int) -> Int -> [Int] -> Int
+  calcular f e l = calcular' l
+    where calcular' [] = e
+          calcular' (x:xs) = f x (calcular' xs)
+
+  res1 = calcular (+) 0 [1..4]
+  res2 = calcular (*) 1 [1..4]
+  res3 = calcular (*) 0 [1..]
+  res4 = calcular (-) 0 [1..4]
+  res5 = calcular f 0 [1..4]
+    where f x = (*2) . (+) x
