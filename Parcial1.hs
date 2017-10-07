d3c3b218491077895ea50071a0a2f410cd09d8e1
Yerguer <dariohdez16@gmail.com>
2017-10-07 11:58:49 +0200

    Parcial1 ejercicio2 done

--- Parcial1.hs
+++ Parcial1.hs
@@ -11,3 +11,10 @@ module Parcial1 where
   res4 = calcular (-) 0 [1..4]
   res5 = calcular f 0 [1..4]
     where f x = (*2) . (+) x
+
+  calcular' :: (Int -> Int -> Int) -> Int -> [Int] -> Int
+  calcular' f e l
+      | l <= []   = e
+      | otherwise = f x (calcular' f e xs)
+      where x = head l
+            xs = tail l
