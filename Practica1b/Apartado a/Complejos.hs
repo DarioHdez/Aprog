66bbbf098afe1d1058d7b22de1c8b5274a87dc35
Yerguer <dariohdez16@gmail.com>
2017-10-19 14:49:26 +0200

    Cosos

--- Practica1b/Apartado a/Complejos.hs
+++ Practica1b/Apartado a/Complejos.hs
@@ -1,75 +1,66 @@
 module Complejos where
 
-  --data Complejo = Complejo { real :: Double ,
-    --                         imaginario :: Double
-      --                     } deriving (Show)
+ type Complejo = (Double,Double)
 
-  --sumaComplejo :: Complejo a => a -> a -> a
-  --sumaComplejo x y = (n,m) where n = (+) (fst x) (fst y)
-    --                             m = (+) (snd x) (snd y)
+ sumaComplejos :: [Complejo] -> Complejo
+ sumaComplejos [] = (0,0)
+ sumaComplejos (x:xs) = sumaComplejos_base x (sumaComplejos xs)
 
-
- a :: (Double, Double)
- a = (1,3)
-
- b :: (Double,Double)
- b = (2,5)
-
- sumaComplejos :: (Double,Double) -> (Double,Double) -> (Double,Double)
- sumaComplejos x y = (n,m)
+ sumaComplejos_base :: Complejo -> Complejo -> Complejo
+ sumaComplejos_base x y = (n,m)
                      where n = (+) (fst x) (fst y)
                            m = (+) (snd x) (snd y)
 
- multiplicarComplejos :: (Double,Double) -> (Double,Double) -> (Double,Double)
+ multiplicarComplejos :: Complejo -> Complejo -> Complejo
  multiplicarComplejos x y = (n,m)
                             where n = (*) (fst x) (fst y)
                                   m = (*) (snd x) (snd y)
 
- restarComplejos :: (Double,Double) -> (Double,Double) -> (Double,Double)
+ restarComplejos :: Complejo -> Complejo -> Complejo
  restarComplejos x y = (n,m)
                        where n = (-) (fst x) (fst y)
                              m = (-) (snd x) (snd y)
 
- moduloComplejos :: (Double,Double) -> Double
+ moduloComplejos :: Complejo -> Double
  moduloComplejos x = sqrt y where y = (fst x)^^2+(snd x)^^2
 
- convertirACadena :: (Double,Double) -> String
+ convertirACadena :: Complejo -> String
  convertirACadena x = " (" ++ show (fst x) ++ "," ++ show (snd x) ++ ") "
 
- convertirListaACadena :: [(Double,Double)] -> String
+ convertirListaACadena :: [Complejo] -> String
  convertirListaACadena [] = " "
  convertirListaACadena (x:xs) = convertirACadena x ++ convertirListaACadena xs
 
- idNegativo :: (Double,Double) -> Bool
+ idNegativo :: Complejo -> Bool
  idNegativo x = if (fst x) < 0 && (snd x) < 0 then True
                 else False
 
- idListaNegativo :: [(Double,Double)] -> [(Double,Double)]
+ idListaNegativo :: [Complejo] -> [Complejo]
  idListaNegativo [] = []
  idListaNegativo (x:xs) | (idNegativo x == True) = [x] ++ idListaNegativo xs
                         | otherwise = idListaNegativo xs
 
- quitarCero :: [(Double,Double)] -> [(Double,Double)]
+ quitarCero :: [Complejo] -> [Complejo]
  quitarCero [] = []
  quitarCero (x:xs) | (fst x) == 0 && (snd x) == 0 = quitarCero xs
                    | otherwise = [x] ++ quitarCero xs
 
- sortByAscendantModule :: [(Double,Double)] -> [(Double,Double)]
+ sortByAscendantModule :: [Complejo] -> [Complejo]
  sortByAscendantModule [x] = [x]
  sortByAscendantModule (x:xs) = bubble x (sortByAscendantModule xs)
 
- bubble :: (Double,Double) -> [(Double,Double)] -> [(Double,Double)]
+ bubble :: Complejo -> [Complejo] -> [Complejo]
  bubble y [] = [y]
  bubble y (x:xs) | moduloComplejos y > moduloComplejos x = [x] ++ bubble y xs
                  | otherwise = [y] ++ [x] ++ xs
 
- complexDistributed :: [(Double,Double)] -> [(Double,Double)] -> [(Double,Double)]
+ complexDistributed :: [Complejo] -> [Complejo] -> [Complejo]
  complexDistributed [] [] = []
  complexDistributed xs [] = []
  complexDistributed [] ys = []
  complexDistributed [x] ys = runYs x ys
  complexDistributed (x:xs) ys = (complexDistributed xs ys) ++ (runYs x ys)
 
- runYs :: (Double,Double) -> [(Double,Double)] -> [(Double,Double)]
+ runYs :: Complejo -> [Complejo] -> [Complejo]
  runYs x [] = []
  runYs x ys = [(multiplicarComplejos x (head ys))] ++ (runYs x (tail ys))
--- Practica1b/Apartado b/Complejos.hs
+++ Practica1b/Apartado b/Complejos.hs
@@ -1,75 +1,26 @@
 module Complejos where
 
-  --data Complejo = Complejo { real :: Double ,
-    --                         imaginario :: Double
-      --                     } deriving (Show)
+  data Complejo = Cero | Uno | I | Car Double Double | Pol Double Double deriving Show
 
-  --sumaComplejo :: Complejo a => a -> a -> a
-  --sumaComplejo x y = (n,m) where n = (+) (fst x) (fst y)
-    --                             m = (+) (snd x) (snd y)
+  real :: Complejo -> Double
+  real (Car x y) = x
 
+  imaginaria :: Complejo -> Double
+  imaginaria (Car x y) = y
 
- a :: (Double, Double)
- a = (1,3)
+  conversion :: Complejo -> Complejo
+  conversion Cero = (Car 0 0)
+  conversion Uno = (Car 1 0)
+  conversion I = (Car 0 1)
+  conversion (Car x y) = (Car x y)
+  conversion (Pol x y) = (Car a b) where a = (*) x (cos y)
+                                         b = (*) x (sin y)
 
- b :: (Double,Double)
- b = (2,5)
+  sumaComplejos :: [Complejo] -> Complejo
+  sumaComplejos [] = (Car 0 0)
+  sumaComplejos (x:xs) =  (conversion x) +++ (sumaComplejos xs)
 
- sumaComplejos :: (Double,Double) -> (Double,Double) -> (Double,Double)
- sumaComplejos x y = (n,m)
-                     where n = (+) (fst x) (fst y)
-                           m = (+) (snd x) (snd y)
-
- multiplicarComplejos :: (Double,Double) -> (Double,Double) -> (Double,Double)
- multiplicarComplejos x y = (n,m)
-                            where n = (*) (fst x) (fst y)
-                                  m = (*) (snd x) (snd y)
-
- restarComplejos :: (Double,Double) -> (Double,Double) -> (Double,Double)
- restarComplejos x y = (n,m)
-                       where n = (-) (fst x) (fst y)
-                             m = (-) (snd x) (snd y)
-
- moduloComplejos :: (Double,Double) -> Double
- moduloComplejos x = sqrt y where y = (fst x)^^2+(snd x)^^2
-
- convertirACadena :: (Double,Double) -> String
- convertirACadena x = " (" ++ show (fst x) ++ "," ++ show (snd x) ++ ") "
-
- convertirListaACadena :: [(Double,Double)] -> String
- convertirListaACadena [] = " "
- convertirListaACadena (x:xs) = convertirACadena x ++ convertirListaACadena xs
-
- idNegativo :: (Double,Double) -> Bool
- idNegativo x = if (fst x) < 0 && (snd x) < 0 then True
-                else False
-
- idListaNegativo :: [(Double,Double)] -> [(Double,Double)]
- idListaNegativo [] = []
- idListaNegativo (x:xs) | (idNegativo x == True) = [x] ++ idListaNegativo xs
-                        | otherwise = idListaNegativo xs
-
- quitarCero :: [(Double,Double)] -> [(Double,Double)]
- quitarCero [] = []
- quitarCero (x:xs) | (fst x) == 0 && (snd x) == 0 = quitarCero xs
-                   | otherwise = [x] ++ quitarCero xs
-
- sortByAscendantModule :: [(Double,Double)] -> [(Double,Double)]
- sortByAscendantModule [x] = [x]
- sortByAscendantModule (x:xs) = bubble x (sortByAscendantModule xs)
-
- bubble :: (Double,Double) -> [(Double,Double)] -> [(Double,Double)]
- bubble y [] = [y]
- bubble y (x:xs) | moduloComplejos y > moduloComplejos x = [x] ++ bubble y xs
-                 | otherwise = [y] ++ [x] ++ xs
-
- complexDistributed :: [(Double,Double)] -> [(Double,Double)] -> [(Double,Double)]
- complexDistributed [] [] = []
- complexDistributed xs [] = []
- complexDistributed [] ys = []
- complexDistributed [x] ys = runYs x ys
- complexDistributed (x:xs) ys = (complexDistributed xs ys) ++ (runYs x ys)
-
- runYs :: (Double,Double) -> [(Double,Double)] -> [(Double,Double)]
- runYs x [] = []
- runYs x ys = [(multiplicarComplejos x (head ys))] ++ (runYs x (tail ys))
+  (+++) :: Complejo -> Complejo -> Complejo
+  (+++) x y = (Car n m)
+                      where n = (+) (real x) (real y)
+                            m = (+) (imaginaria x) (imaginaria y)
