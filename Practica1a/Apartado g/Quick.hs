b82169238ef9f140fd5d30c0881d12ff7f38ebc9
Yerguer <dariohdez16@gmail.com>
2017-10-25 17:35:27 +0200

    1a-g done

--- Practica1a/Apartado g/Quick.hs
+++ Practica1a/Apartado g/Quick.hs
@@ -1,6 +1,8 @@
 module Quicksort where
 
-
+  -- Quicksort implementado sin listas por comprension usando la funcion
+  --  filter. La recursividad se mantiene, pero para sacar los numeros menores
+  --  y mayores usamos filter con <= y >
   quicksort :: Ord t => [t] -> [t]
   quicksort [] = []
   quicksort (x:xs) = quicksort (filter (<=x) xs) ++ [x] ++ quicksort (filter (>x) xs)
