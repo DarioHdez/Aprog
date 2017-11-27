c1e07d3ba4bc3f515659964736f59330520c4564
Yerguer <dariohdez16@gmail.com>
2017-11-27 11:46:03 +0100

    pues ahi vamos

---
+++ Practica2a/Apartado_a.hs
@@ -0,0 +1,107 @@
+module Tautologias where
+
+-- Tautology checker --
+-----------------
+
+ data Prop = Const Bool | Var Char | Not Prop | And Prop Prop | Imply Prop Prop | Or Prop Prop | DImply Prop Prop  deriving Show
+
+ type Subst =  Assoc Char Bool
+
+ type Assoc k v =  [(k,v)]
+
+ p1 :: Prop
+ p1 = And (Var 'A') (Not (Var 'A'))
+
+ p2 :: Prop
+ p2 = Imply  (And (Var 'A') (Var 'B')) (Var 'A')
+
+ (¬) :: Prop -> Prop
+ (¬) = Not
+
+ (-->) :: Prop -> Prop -> Prop
+ (-->) = Imply
+
+ (<-->) :: Prop -> Prop -> Prop
+ --(<-->) a b = And (Imply a b) (Imply b a)
+ (<-->) = DImply
+
+ (/\) :: Prop -> Prop -> Prop
+ (/\)  = And
+
+ (\/) :: Prop -> Prop -> Prop
+ --(\/) a b = And (Not a) (Not b)
+ (\/) = Or
+
+
+ find :: Eq k => k -> Assoc k v -> v
+ find k t =  head [v | (k',v) <- t, k == k']
+
+ eval :: Subst -> Prop -> Bool
+ eval _ (Const b)  =  b
+ eval s (Var x)  =  find x s
+ eval s (Not p) = not (eval s p)
+ eval s (And p q) =  eval s p && eval s q
+ eval s (Imply p q) =  eval s p <= eval s q
+ eval s (Or p q) = eval s p || eval s q
+ eval s (DImply p q) = eval s (Imply p q) && eval s (Imply q p)
+
+ vars :: Prop -> [Char]
+ vars (Const _) =  []
+ vars (Var x) = [x]
+ vars (Not p) =  vars p
+ vars (And p q) =  vars p ++ vars q
+ vars (Imply p q) =  vars p ++ vars q
+ vars (Or p q) = vars p ++ vars q
+ vars (DImply p q) = vars p ++ vars q
+
+ bools :: Int -> [[Bool]]
+ bools 0 =  [[]]
+ bools n =  map (False:) bss ++ map (True:) bss
+              where bss = bools (n-1)
+
+ rmdups :: Eq a => [a] -> [a]
+ rmdups [] =  []
+ rmdups (x:xs) =  x : rmdups (filter (/= x) xs)
+
+ substs :: Prop -> [Subst]
+ substs p =  map (zip vs) (bools (length vs))
+              where vs = rmdups (vars p)
+
+ isTaut :: Prop -> Bool
+ isTaut p =  and [eval s p | s <- substs p]
+
+ toString :: Prop -> String
+ toString (Const x) = show x
+ toString (Var x) = [x]
+ toString (Not x) =  "(¬" ++ toString x ++ ")"
+ toString (And x y) =  "(" ++ toString x ++ "/\\" ++ toString y ++ ")"
+ toString (Or x y) =  "(" ++ toString x ++ "\\/" ++ toString y ++ ")"
+ toString (Imply x y) = "(" ++ toString x ++ "-->" ++ toString y ++ ")"
+ toString (DImply x y) = "(" ++ toString x ++ "<-->" ++ toString y ++ ")"
+
+ myPrint :: Prop -> IO()
+ myPrint x = putStrLn $ toString x
+
+ --polacaInversa :: Prop -> Bool
+ --polacaInversa x = eval s pInversa x
+
+ pila :: [Prop]
+ pila = []
+
+ pInversa :: [Char] -> [Prop]
+ pInversa [] = [] -- cambiar
+ --pInversa ("\\/":xs) = --sacar 2
+ --pInversa ("/\\":xs) = -- sacar 2
+ --pInversa ("<-->":xs) =
+ --pInversa ("-->":xs) =
+ pInversa ('¬':xs) = pushProp pila (take1 pila)--sacar 1
+ pInversa (x:xs) = pushVar pila x ++ pInversa xs    -- meter a la pila
+
+ pushVar :: [Prop] -> Char -> [Prop]
+ pushVar xs x = xs ++ [(Var x)]
+
+ pushProp :: [Prop] -> Prop -> [Prop]
+ pushProp xs x = xs ++ [x]
+
+ take1 :: [Prop] -> Prop
+ take1 xs = (Not (last xs))
